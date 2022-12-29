03befec508094a800dbaa8f8263c1fd1683535cf
LenaMaistrenko <istlauder@gmail.com>
2022-12-29 12:27:49 +0200

    Update 03-feedback.js

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -2,8 +2,24 @@ console.log('contactFormEl');
 const contactFormEl = document.querySelector('.feedback-form');
 
 const UserInfo = {};
+// const fillContactFormFields = () => {
+//   const userInfoFromLS = localStorageService.load('userData');
+
+//   if (userInfoFromLS === undefined) {
+//     return;
+//   }
+
+//   for (const prop in userInfoFromLS) {
+//     contactFormEl.elements[prop].value = userInfoFromLS[prop];
+//   }
+// };
+
+//fillContactFormFields();
 contactFormEl.addEventListener('change', formChange);
 function formChange(evt) {
   const { target } = evt;
   console.log(target);
+  const fieldName = target.name;
+  const fieldValue = target.value;
+  console.log(fieldName, fieldValue);
 }
