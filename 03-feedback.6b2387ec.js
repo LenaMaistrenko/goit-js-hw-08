98d7615845a64746289c0432dcd904cc8a3521c3
LenaMaistrenko <116562150+LenaMaistrenko@users.noreply.github.com>
2022-12-29 11:59:34 +0000

    Deploying to gh-pages from @ LenaMaistrenko/goit-js-hw-08@a1e748423cca05c3eb3cf4c2558eaa290e54730c 🚀

---
+++ 03-feedback.6b2387ec.js
@@ -0,0 +1,2 @@
+console.log("contactFormEl"),document.querySelector(".feedback-form").addEventListener("change",(function(e){var o=e.target;console.log(o)}));
+//# sourceMappingURL=03-feedback.6b2387ec.js.map
