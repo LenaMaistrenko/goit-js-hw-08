334597c0e22d1df1a5c5dfb9eb951a3c4a79ae3b
LenaMaistrenko <116562150+LenaMaistrenko@users.noreply.github.com>
2022-12-29 18:51:33 +0000

    Deploying to gh-pages from @ LenaMaistrenko/goit-js-hw-08@aa6d9ae5cdf112149bc02ed57f4cf94ac558108c 🚀

--- 03-feedback.2ab1dfe7.js
+++
@@ -1,2 +0,0 @@
-console.log("contactFormEl"),document.querySelector(".feedback-form").addEventListener("change",(function(e){var o=e.target;console.log(o);var n=o.name,c=o.value;console.log(n,c)}));
-//# sourceMappingURL=03-feedback.2ab1dfe7.js.map
--- 03-feedback.994caff3.js
+++
@@ -1,2 +0,0 @@
-console.log("contactFormEl");document.querySelector(".feedback-form").addEventListener("change",(function(o){const{target:e}=o;console.log(e);const n=e.name,c=e.value;console.log(n,c)}));
-//# sourceMappingURL=03-feedback.994caff3.js.map
